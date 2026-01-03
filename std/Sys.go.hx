7e700d30e3c9147d35c8ebed16411bd3ee75ae9f
Joey <9813078+PXshadow@users.noreply.github.com>
2026-01-04 00:55:43 +0100

    std/Sys: use generic to catch any value, without it being wrapped as an any interface value

--- std/Sys.go.hx
+++ std/Sys.go.hx
@@ -1,6 +1,6 @@
 class Sys {
-    public static function println(s:Dynamic)
-        go.Syntax.code("println({0})", s);
-    public static function print(s:Dynamic)
-        go.Syntax.code("print({0})", s);
+    public static function println<Type>(v:Type)
+        go.Syntax.code("println({0})", v);
+    public static function print<Type>(v:Type)
+        go.Syntax.code("print({0})", v);
 }
