18955bd8360b9726c24b51630458dd7a9563c475
Joey <9813078+PXshadow@users.noreply.github.com>
2026-01-05 13:30:22 +0100

    Sys.go.hx: Type -> T

--- std/Sys.go.hx
+++ std/Sys.go.hx
@@ -1,6 +1,6 @@
 class Sys {
-    public static function println<Type>(v:Type)
+    public static function println<T>(v:T)
         go.Syntax.code("println({0})", v);
-    public static function print<Type>(v:Type)
+    public static function print<T>(v:T)
         go.Syntax.code("print({0})", v);
 }
