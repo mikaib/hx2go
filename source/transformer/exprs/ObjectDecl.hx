b5ff8c0d1bd9992a4574bd53f30c94af2840f341
Joey <9813078+PXshadow@users.noreply.github.com>
2026-01-18 18:15:20 +0100

    transformer: add ObjectDecl

---
+++ source/transformer/exprs/ObjectDecl.hx
@@ -0,0 +1,48 @@
+package transformer.exprs;
+
+import HaxeExpr.HaxeExprDef;
+import haxe.macro.Expr.ComplexType;
+import HaxeExpr.HaxeObjectField;
+import HaxeExpr.HaxeField;
+
+function transformObjectDecl(t:Transformer, fields:Array<HaxeObjectField>, ct:ComplexType):HaxeExprDef {
+    transformObjectDeclTypes(t, fields, ct);
+    final list:Array<HaxeExpr> = [];
+    trace("fields len:", fields.length);
+    for (field in fields) {
+        t.transformExpr(field.expr);
+        // expr -> any(expr)
+        field.expr.def = ECheckType({def: field.expr.def, t: field.expr.t}, TPath({pack: [], name: "any"}));
+        final fieldName:HaxeExpr = {def: EConst(CString(field.field)), t: null};
+        // a => b
+        list.push({def: EBinop(OpArrow, fieldName, field.expr), t: null});
+    }
+    return EArrayDecl(list);
+}
+
+function transformObjectDeclTypes(t:Transformer, fields:Array<HaxeObjectField>, ct:ComplexType) {
+    switch ct {
+        case TAnonymous(anonFields):
+            for (i in 0...anonFields.length) {
+                final ct = switch anonFields[i].kind {
+                    case FVar(varType, _):
+                        varType;
+                    case FFun(f):
+                        TFunction(f.args.map(arg -> arg.type), f.ret);
+                    case FProp(get, set, t, e):
+                        TPath({pack: [], name: "Dynamic"});
+                        
+                };
+                t.transformComplexType(ct);
+                // fields come in unordered
+                for (j in 0...fields.length) {
+                    if (fields[j].field == anonFields[i].name) {
+                        fields[j].t = ct;
+                        break;
+                    }
+                }
+            }
+        default:
+            throw "Not TAnon";
+    }
+}
