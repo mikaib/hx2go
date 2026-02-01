d36cb311dc497aee17baa427b67cb5831c69d1d2
mikaib <mikaib@duck.com>
2026-02-01 17:55:21 +0100

    push uncommited files

--- tests/issues/Issue37.hx
+++ tests/issues/Issue37.hx
@@ -5,15 +5,13 @@ import go.Go;
 function run() {
     var i: go.GoInt = 12;
     var f: Float = 12.1;
-
     assert(i == f, false);
 
     var hi: Int = 15;
     var hf: Float = 20;
+    assert(hi == hf, false);
 
     var i32 = Go.int32(10);
     var u32 = Go.uint32(10);
-
-    assert(hi == hf, false);
     assert(i32 == u32, true);
 }
