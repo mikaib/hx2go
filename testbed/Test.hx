8ef901d50662a636a1b4ee792a42091d052d6b47
Joey <9813078+PXshadow@users.noreply.github.com>
2026-02-14 14:14:28 +0100

    testbed: update testbed case to show appending to string end result with Int and Float as rhs binops

--- testbed/Test.hx
+++ testbed/Test.hx
@@ -1,28 +1,3 @@
-class Test {
-	public static function main() {
-		var i:Int = 15;
-		var f:Float = 20.3;
-
-		var i32 = Go.int32(10);
-		var u32 = Go.uint32(10);
-
-		Sys.println(i == f);
-		Sys.println(i32 == u32);
-
-		var aI:Int = -1;
-		var bI:Int = 1;
-		Sys.println((aI >>> bI : Int));
-
-		var f_ = Std.int(f);
-
-		Sys.println(f_);
-
-		var parsed = Std.parseInt("1");
-		Sys.println(parsed);
-		var parsed_float = Std.parseFloat("1.01");
-		Sys.println(parsed_float);
-
-		var rand_num = Std.random(100);
-		Sys.println('Random number: ${Std.string(rand_num)}');
-	}
+function main() {
+	Sys.println("hello" + 10 + "hi" + 10.3);
 }
